8467008ed055cb340604f1b40961f3fb4550dfc7
Michael Mahachi <Michael.Mahachi@derivco.co.za>
2022-04-28 12:25:30 +0200

    Dynamic Types - Functons

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-let selectedColors = ['red','blue'];
-selectedColors[2] = 1;
-console.log(selectedColors.length);
+
+function greet(name, lastName){
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+greet('John','Smith');
