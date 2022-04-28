e87761726221a3c2a465f1fdc2e2b036af8f7221
Michael Mahachi <Michael.Mahachi@derivco.co.za>
2022-04-28 12:32:18 +0200

    Types of functions

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-
+// Performing a task
 function greet(name, lastName){
     console.log('Hello ' + name + ' ' + lastName);
 }
 
-greet('John','Smith');
+// Calculating a value
+function square(number){
+   return number * number
+}
+
+//greet('John','Smith');
+
+let number = square(2);
+console.log(number);
